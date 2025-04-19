0e802450b801c650588e3136e23337f4b837d0af
Ken Muchiri <kennethmuchiri7168@gmail.com>
2025-04-19 13:13:39 +0300

    google sign fixed

--- healthiq_flutter/android/build.gradle.kts
+++ healthiq_flutter/android/build.gradle.kts
@@ -6,7 +6,7 @@ allprojects {
 }
 plugins {
     // ...
-
+    kotlin("android") version "2.1.0" apply false
     // Add the dependency for the Google services Gradle plugin
     id("com.google.gms.google-services") version "4.4.2" apply false
 
